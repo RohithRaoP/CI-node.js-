de437bee0db3ef31e18551846bb727a29736ced5
RohithRaoP <rohithraopogala3@gmail.com>
2026-03-25 16:27:51 +0530

    Increase minimum password length from 8 to 9

--- src/password.js
+++ src/password.js
@@ -1,7 +1,7 @@
 function isValidPassword(password) {
   if (!password) return false;
 
-  const minLength = 8;
+  const minLength = 9;
   const hasUpperCase = /[A-Z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
 

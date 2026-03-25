7b4cfa93779b3d04daf5ba89c05cb8506cd505d4
RohithRaoP <rohithraopogala3@gmail.com>
2026-03-25 12:37:45 +0530

    Add tests for password validation

--- tests/password.test.js
+++ tests/password.test.js
@@ -1,1 +1,17 @@
+const { isValidPassword } = require('../src/password');
 
+test('valid password', () => {
+  expect(isValidPassword('Password1')).toBe(true);
+});
+
+test('too short', () => {
+  expect(isValidPassword('Pass1')).toBe(false);
+});
+
+test('no uppercase', () => {
+  expect(isValidPassword('password1')).toBe(false);
+});
+
+test('no number', () => {
+  expect(isValidPassword('Password')).toBe(false);
+});

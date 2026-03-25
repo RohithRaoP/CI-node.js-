1e3031cbc93a714069ececd92ef11dc714e33e71
RohithRaoP <rohithraopogala3@gmail.com>
2026-03-25 12:37:07 +0530

    Add password validation function

--- src/password.js
+++ src/password.js
@@ -1,1 +1,15 @@
+function isValidPassword(password) {
+  if (!password) return false;
 
+  const minLength = 8;
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+
+  return (
+    password.length >= minLength &&
+    hasUpperCase &&
+    hasNumber
+  );
+}
+
+module.exports = { isValidPassword };
